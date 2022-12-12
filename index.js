2d42775f93cdbd655c7a1a174807551f4c405dfc
IKBOstudent <90282668+IKBOstudent@users.noreply.github.com>
2022-12-12 19:14:38 +0300

    fix vBeta

--- index.js
+++ index.js
@@ -3,29 +3,30 @@ import fs from "fs";
 import mongoose from "mongoose";
 import multer from "multer";
 import cors from "cors";
+import "dotenv/config";
 
-import { loginValidation, registerValidation, blogValidation } from "./validations.js";
+import { loginValidation, registerValidation, blogValidation } from "./utils/validations.js";
 
 import { checkAuth, handleValidationErrors } from "./utils/index.js";
 import { getHome, UserControllers, BlogControllers } from "./controllers/index.js";
 
 mongoose
-  .connect(String(process.env.MONGO_URI))
-  .then(() => console.log("DATABASE OK"))
-  .catch((err) => console.log("DATABASE FAILED", err));
+    .connect(String(process.env.MONGO_URI))
+    .then(() => console.log("DATABASE OK"))
+    .catch(err => console.log("DATABASE FAILED", err));
 
 const app = express();
 
 const storage = multer.diskStorage({
-  destination: (_, __, cb) => {
-    if (!fs.existsSync("uploads")) {
-      fs.mkdirSync("uploads");
-    }
-    cb(null, "uploads");
-  },
-  filename: (_, file, cb) => {
-    cb(null, file.originalname);
-  },
+    destination: (_, __, cb) => {
+        if (!fs.existsSync("uploads")) {
+            fs.mkdirSync("uploads");
+        }
+        cb(null, "uploads");
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
 });
 
 const upload = multer({ storage });
@@ -39,9 +40,9 @@ app.get("/", getHome);
 
 // UPLOAD
 app.post("/upload", checkAuth, upload.single("image"), (request, response) => {
-  response.json({
-    url: `/uploads/${request.file.originalname}`,
-  });
+    response.json({
+        url: `/uploads/${request.file.originalname}`,
+    });
 });
 
 // AUTH
@@ -61,10 +62,10 @@ app.delete("/posts/:id", checkAuth, BlogControllers.remove);
 app.patch("/posts/:id", checkAuth, blogValidation, handleValidationErrors, BlogControllers.update);
 app.patch("/posts/:id/comment", checkAuth, BlogControllers.newCommentUpdate);
 
-const PORT = process.env.PORT || 4444;
-app.listen(PORT, (err) => {
-  if (err) {
-    return console.log("RUN FAILED", err);
-  }
-  console.log("SERVER OK");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, err => {
+    if (err) {
+        return console.log("RUN FAILED", err);
+    }
+    console.log("SERVER IS RUNNING ON PORT " + PORT);
 });
