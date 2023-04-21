bf2912a6a7102ff96a15e3a427204fe22ad1d2f6
IKBOstudent <maxpon225@gmail.com>
2023-04-21 23:04:11 +0300

    v3.0.1 ignore changed

--- dist/controllers/mainController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getHome = void 0;
-const getHome = (request, response) => {
-    response.send("server is running...");
-};
-exports.getHome = getHome;
-//# sourceMappingURL=mainController.js.map
